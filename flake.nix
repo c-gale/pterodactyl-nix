8cbacc2155bbe35f7bb14a59f751a02f7a6b8c56
c-gale <cubylive@gmail.com>
2026-01-17 02:51:49 -0700

    fix

--- flake.nix
+++ flake.nix
@@ -2,18 +2,27 @@
   description = "Nixos modules for the pterodactyl game server panel";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
-    nixosModules = nixpkgs.lib.genAttrs [ "pterodactyl" ] (
-      module: import ./modules/${module}.nix { flake = self; }
-    );
+  outputs = { self, nixpkgs, ... }:
+    let
+      systems = [ "x86_64-linux" ];
+    in
+    {
+      # export modules under self.nixosModules
+      nixosModules = nixpkgs.lib.genAttrs [ "pterodactyl" ] (module: 
+        import ./modules/${module}.nix { inherit self nixpkgs; }
+      );
 
-    packages = nixpkgs.lib.genAttrs [ "x86_64-linux" ] (
-      system: nixpkgs.lib.genAttrs [ "pterodactyl" "php" "wings" ] (
-        package: import ./packages/${package}.nix { pkgs = import nixpkgs { inherit system; }; };
-      )
-    });
-  };
+      # export packages for each system
+      packages = nixpkgs.lib.genAttrs systems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        nixpkgs.lib.genAttrs [ "pterodactyl" "php" "wings" ] (pkg:
+          import ./packages/${pkg}.nix { inherit pkgs; }
+        )
+      );
+    };
 }
