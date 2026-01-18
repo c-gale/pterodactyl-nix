58c6c14c65a06b31d3cdceff221ba5da0bf1c476
c-gale <cubylive@gmail.com>
2026-01-18 16:46:54 -0700

    asdsa

--- flake.nix
+++ flake.nix
@@ -7,6 +7,7 @@
 
   outputs = { self, nixpkgs }: 
       let
+        system = "x86_64-linux";
         pkgs = import nixpkgs { inherit system; };
       in {
         packages.pterodactylPanel = pkgs.callPackage ./default.nix {};
